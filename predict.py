99eeb1fb71c791fb5f3fd62a141bcd2c79a66680
Malak Bayoumy <malak.bayoumy41@gmail.com>
2025-05-19 08:28:20 +0000

    done

---
+++ predict.py
@@ -0,0 +1,28 @@
+import mlflow.pyfunc
+import pandas as pd
+import dagshub
+import mlflow
+
+# Initialize Dagshub tracking
+dagshub.init(repo_owner='malak.bayoumy41',
+             repo_name='python-scripts',
+             mlflow=True)
+
+# Load the model from the Production stage
+model_uri = "models:/titanic/2"
+model = mlflow.pyfunc.load_model(model_uri)
+
+# Sample input
+sample_input = pd.DataFrame({
+    "Pclass": [3],
+    "Sex": ["male"],
+    "SibSp": [1],
+    "Parch": [0],
+    "Age": [22.0],
+    "Fare": [7.25],
+    "Embarked": ["S"]
+})
+
+# Predict
+predictions = model.predict(sample_input)
+print(predictions)
